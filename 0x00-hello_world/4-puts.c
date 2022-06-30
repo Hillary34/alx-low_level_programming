7c03bcb3fde529bcea09208f4b5c7e5ad941502d
Hillary34 <ezemakenechukwu@yahoo.com>
2022-06-30 16:29:59 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/
+* main -Entry point
+* Return: Always O (sucess)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (O);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-a script that compiles a c file and creates an executable file cisfun
+script that prints out a text
