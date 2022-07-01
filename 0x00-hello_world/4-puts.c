a21497889d485a535e616e8f353dc73011b3812d
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-01 13:26:00 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-/
+#include <string.h>
+/**
 * main - Entry point
 * Return: Always 0 (sucess)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+char items[] = "\"Programming is like building a multilingual puzzle";
+puts(items);
 return (0);
 }
