c9a223d67ee411d64ed6d237c769e2e42a6d5c33
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-03 15:55:05 -0700

    my commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,9 +7,7 @@
 */
 int main(void)
 {
-int d;
-int p;
-int q;
+int d, p, q;
 for (d = '0'; d < '9'; d++)
 {
 for (p = d + 1; p <= '9'; p++)
@@ -17,13 +15,13 @@ for (p = d + 1; p <= '9'; p++)
 for (q = p + 1; q <= '9'; q++)
 {
 if ((p != d) != q)
-{				
-putchar(d);			
-putchar(p);			
+{
+putchar(d);
+putchar(p);
 putchar(q);
-if (d == '7' && p == '8')	
+if (d == '7' && p == '8')
 continue;
-putchar(',');			
+putchar(',');
 putchar(' ');
 }
 }
