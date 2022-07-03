61b9b3b8488ce1a3699ad4e65d04c2a416bd115c
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-03 15:34:19 -0700

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,21 @@
 */
 int main(void)
 {
-int d;
-for (d = 0; d < 100; d++)
+int d, p;
+for (d = '0'; d < '9'; d++)
 {
-putchar((d / 10) + '0');
-putchar((d % 10) + '0');
-}
-if (d != 99)
+for (p = d + 1; p <= '9'; p++)
 {
+if (p != d)
+{
+putchar(d);
+putchar(p);
+if (d == '8' && p == '9')	
+continue;
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
