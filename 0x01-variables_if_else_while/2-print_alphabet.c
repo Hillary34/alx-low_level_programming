16892857d4b0d85b59af8a17d16f43f8e9461b3b
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-03 04:06:23 -0700

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
+* ain - Entry point
+* Return: Always 0 (Success)
 */
-int main(void)[A[A[A[A[#[D[#[D[D[D[#[B[B[B[B [D[B Return: A[C[Dlways 0 (Success)[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[B[B[B[B[B[A[A[A[A[A[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D{
+int main(void)
+{
 char low;
 for (low = 'a'; low <= 'z'; low++)
 putchar(low);
 putchar('\n');
 return (0);
 }
+[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[B[B[B[Bmain - Entry point[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B
