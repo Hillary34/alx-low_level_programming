a5027fbfca23ee384f55262757dad1954a4b21a1
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-03 17:09:29 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
-#include <stdio.h>                                                                                              
-/**                                                                                                             
-* main - Entry point                                                                                            
-* Return: Always 0 (Success)                                                                                    
-*/                                                                                                              
-{                                                                                                               
-int i;                                                                                                          
-long int l;                                                                                                     
-long long int ll;                                                                                               
-float d;                                                                                                        
-char c;                                                                                                         
-printf(Size of a char : % lu byte(s)\n", (unsigned long)sizeof(c));                                               
-printf(Size of an int : % lu byte(s)\n", (unsigned long)sizeof(i));                                               
-printf(Size of a long int : % lu byte(s)\n", (unsigned long)sizeof(l));                                           
-printf(Size of a long long int : % lu byte(s)\n", (unsigned long)sizeof(ll));                                     
-printf(Size of a float : % lu byte(s)\n", (unsigned long)sizeof(d));                                              
-return (0);                                                                                                     
-} 
+#include <stdio.c>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+{
+int i;
+long int a;
+long long int b;
+float d;
+char c;
+printf(Size of a char: %lu bytes\n", sizeof(c));
+printf(Size of a [B[B[B[B[B[A[A[A[A int: *lu bytes\n", size[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[%[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[Dof(i));[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cchar: %lu bytes\n", sizeof(c));[B[B[B[A[A[D[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dprintf(Size of long int: %lu bytes\n", sizeof(a));
+printf(Size of long long int: %lu bytes\n", sizeof(b));
+printf(Size of float: %lu bytes\n", sizeof(d));
+return (0);
+}
