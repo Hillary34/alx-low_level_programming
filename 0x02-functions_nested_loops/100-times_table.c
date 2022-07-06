978a8af7530d239d748a674118f7ef78a9ed104d
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-06 06:52:00 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,40 +13,26 @@ for (i = 0; i <= n; i++)
 {
 _putchar('0');
 for (j = 1; j <= n; j++)
-putformat(i * j);
+{
+_putchar(',');
+_putchar(' ');
+prod = i * j;
+if (prod <= 99)
+_putchar(' ');
+if (prod <= p)
+_putchar(' ');
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
 _putchar('\n');
 }
-}
-}
-/**
-* putformat - formatted characters to output
-* @n: number to format
-* Return: nothing
-*/
-void putformat(int n)
-{
-if (n <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(n + '0');
-}
-else if (n > 9 && n <= 99)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(n / 100 + '0');
-_putchar(n / 10 % 10 + '0');
-_putchar(n % 10 + '0');
 }
 }
