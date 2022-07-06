b0358483cb15757d371f7b2e819cc42bc82afcb0
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-06 03:01:22 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 /**
 * main - Check description
 * Description: It prints the word _putchar, followed by a new line.
-* Return: 0.
+* Return: 0 (Success)
 */
 int main(void)
 {
-char word[8] = "_putchar";
+char word[7] = "putchar";
 int i;
-for (i = 0; i < 8; i++)
+for (i = 0; i < 7; i++)
 _putchar(word[i]);
 _putchar('\n');
 return (0);
 }
+
