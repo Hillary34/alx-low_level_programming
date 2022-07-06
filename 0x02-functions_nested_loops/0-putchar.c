b746eb42ba999a347f4935b4bdf660447c92d246
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-06 02:48:44 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
-#include <unistd.h>
-#include "_putchar.c"
+#include "main.h"
 /**
 * main - Check description
-* Description: It prints the word Holberton, followed by a new line.
+* Description: It prints the word _putchar, followed by a new line.
 * Return: 0.
 */
 int main(void)
@@ -13,4 +12,4 @@ for (i = 0; i < 8; i++)
 _putchar(word[i]);
 _putchar('\n');
 return (0);
-}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[C[C[C[C[Cmain.h"[B[B[B[B[B[B[B[B[B[B[B[B[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[ _putchar[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C, followed by a new line.[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+}
