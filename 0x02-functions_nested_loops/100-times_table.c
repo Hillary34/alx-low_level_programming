655dd020f44974f38935434e743e632003150194
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-06 19:14:03 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B#include <stdio.h>
 /**
 * print_to_98 - Prints all natural numbers from input to 98 in order seperated b* y a comma followed by a space.
 * @n: The number to begin counting at.
@@ -12,8 +12,8 @@ printf("%d,", n--);
 printf("%d\n", n);
 }
 else
-[
-while (n <[A[D[D[D[D[D[D[D[D[D[D[{[B[C[C[C[C[C[C[C[C[C[C[C[D[D[C98)
+{
+while (n < 98)
 printf("%d,", n++);
 printf("%d\n", n);
 }
