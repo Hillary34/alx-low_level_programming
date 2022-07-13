d7cd699988a2dc5868fc4cb7246063057f0ad124
Hillary34 <ezemakenechukwu@yahoo.com>
2022-07-12 18:19:12 -0700

    my commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,8 +9,23 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-char *aux = dest;
-while (*src)
-*dest++ = *src++;
+int n = 0;
+int i;
+while (src[n] != '\0')
+{
+n++;
+}
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+i = i;
+while (i <= n)
+{
+dest[i] = '\0';
+i++;
+}
 return (dest);
 }
