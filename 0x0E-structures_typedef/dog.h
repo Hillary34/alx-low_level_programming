f79072a859330998907a2b25dc94ce1401e91783
Hillary34 <ezemakenechukwu@yahoo.com>
2022-08-11 19:49:18 -0400

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
+#ifndef _DOG
+#define _DOG
 /**
 * struct dog - structure for a basic dog
 * @name: Dog's name
@@ -19,4 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endi
+#endif
